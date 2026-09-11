8763a60368de5e1ad05ccdbaf5e66aafd1183289
Blacktwisterworld <ankitsinghjain2651@gmail.com>
2026-09-12 04:10:02 +0530

    Delete test_webhook.py

--- test_webhook.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-token = "8274980094:AAFOCAHI_9c1JykEFqkh2ftDoDPNKYaOU4A"
-url = f"https://api.telegram.org/bot{token}/getWebhookInfo"
-
-response = requests.get(url)
-print("Telegram Webhook Info:")
-print(json.dumps(response.json(), indent=2))
